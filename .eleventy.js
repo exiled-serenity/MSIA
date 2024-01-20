e0076b5574a5596e5e4a61f72e6e1923167616ed
unknown <23489441+exiled-serenity@users.noreply.github.com>
2024-01-19 21:29:48 -0500

    File Passthrough Implemented

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css")
+    eleventyConfig.addPassthroughCopy("images")
+    return {
+        dir: {
+            input: "src",
+            output: "docs"
+        }
+    }
+};
